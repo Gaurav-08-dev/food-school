f1c08defd3f77ccd0e18ee29b995867c093b7ef9
gaurav <g8111997@gmail.com>
2024-08-29 22:06:54 +0530

    coming soon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,10 @@
 import { Header } from "@/sections/Header";
+import Image from "next/image";
 export default function Home() {
-  return <Header/>;
+  return <div className="flex flex-col h-full w-full">
+  <Header/>
+  <div className="absolute left-[45%] top-[50%] ">
+    Coming soon!
+    </div>
+  </div>
 }
